add1b57307c68e1fa5f21d0986b1695f38bbf474
踏可司企鵝 <142492829+proton-penguin@users.noreply.github.com>
2023-10-25 15:26:45 +0000

    Update 金字塔1.py

--- 金字塔1.py
+++ 金字塔1.py
@@ -1,5 +1,3 @@
-def tri():
-    r = range(1,int(input("高度幾層？ "))+1);
-    for i in r:
-        print((len(r)-i)*" "+"* "*i)
-tri()
+r = range(1,int(input("高度幾層？ "))+1);
+for i in r:
+    print((len(r)-i)*" "+"* "*i)
